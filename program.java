3c774c830f609f5da2e230fca8688a490a56ff67
codingdodia <44556214+Dodia2024@users.noreply.github.com>
2021-10-30 13:35:01 -0500

    Update program.java

--- program.java
+++ program.java
@@ -2,6 +2,7 @@ public class program {
     public static void main(String[] args) {
         
         System.out.println("Hello World");
-        system.out.println("waht is up");
+        System.out.println("wht is up");
+        System.out.println("Test");
     }
 }
